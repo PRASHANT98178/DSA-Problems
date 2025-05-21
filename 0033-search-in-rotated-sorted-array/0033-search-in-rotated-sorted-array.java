85228468470831481245960cb2fc3552245b9776
PRASHANT98178 <115872466+PRASHANT98178@users.noreply.github.com>
2025-05-21 10:45:31 +0530

    Time: 0 ms (100%), Space: 42.6 MB (11.72%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,29 @@
 class Solution {
-    public int bs(int[] arr, int lo, int hi, int target){
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                lo=mid+1;
-            }
-            else if(arr[mid]>target){
-                hi=mid-1;
-            }
-        }
-        return -1;
-    }
     public int search(int[] arr, int target) {
         int n = arr.length;
-        int idx = -1;
+        int lo = 0;
+        int hi = n-1;
 
-        for(int i=0; i<n-1; i++){
-            if(arr[i] > arr[i+1]){
-                idx = i;
-                break;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[lo] <= arr[mid]){
+                if(arr[lo] <= target && arr[mid] >= target){
+                    hi = mid-1;
+                }
+                else lo = mid+1;
+            }
+            else{
+                if(arr[mid] <= target && arr[hi] >= target){
+                    lo = mid+1;
+                }
+                else{
+                    hi = mid-1;
+                }
             }
         }
-
-        int left = bs(arr, 0, idx, target);
-        int right = bs(arr, idx+1, n-1, target);
-
-        if(left != -1) return left;
-        if(right != -1) return right;
-
         return -1;
     }
 }
