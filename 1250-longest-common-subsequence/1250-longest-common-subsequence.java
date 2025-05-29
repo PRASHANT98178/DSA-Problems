fab436c17a2832971e0adce11133ff722abad13f
PRASHANT98178 <115872466+PRASHANT98178@users.noreply.github.com>
2025-05-29 16:11:02 +0530

    Time: 28 ms (19.57%), Space: 51 MB (56.33%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -20,11 +20,9 @@ public class Solution {
         int[][] dp = new int[m+1][n+1];
 
        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
-        }
+       for(int[] arr : dp){
+        Arrays.fill(arr, -1);
+       }
 
         return helper(0, 0, text1, text2, dp);
     }
