17a059b333d0135ec69ce6791810b6ef8dbd231d
PRASHANT98178 <115872466+PRASHANT98178@users.noreply.github.com>
2025-05-30 10:26:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pair Sum in a Sorted and Rotated Array/pair-sum-in-a-sorted-and-rotated-array.java
+++ Difficulty: Medium/Pair Sum in a Sorted and Rotated Array/pair-sum-in-a-sorted-and-rotated-array.java
@@ -1,24 +1,30 @@
 
 class Solution {
     static boolean pairInSortedRotated(int arr[], int target) {
-       int n = arr.length;
-       HashMap<Integer, Integer> map = new HashMap<>();
-       
-       for(int i=0; i<n; i++){
-           map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-       }
-       
-        for (int key : map.keySet()) {
-            int complement = target - key;
-
-            if (map.containsKey(complement)) {
-                if (key == complement && map.get(key) >= 2) {
-                    return true;
-                } else if (key != complement) {
-                    return true;
-                }
+        // Your code here
+        int n = arr.length;
+        int pivot = -1;
+        
+        for(int i=0; i<n-1; i++){
+            if(arr[i] > arr[i+1]){
+                pivot = i;
+                break;
             }
         }
+        
+        if(pivot == -1) pivot = n-1;
+        
+        int left = (pivot + 1) % n;
+        int right = pivot;
+        
+        while(left != right){
+            int sum = arr[left] + arr[right];
+            
+            if(sum == target) return true;
+            if(sum < target) left = (left + 1) % n;
+            else right = (n + right - 1) % n;
+        }
+        
         return false;
     }
 }
