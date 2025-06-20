6118b41ade0c6bf8bdb57a5c529e4ad17d073664
PRASHANT98178 <115872466+PRASHANT98178@users.noreply.github.com>
2025-06-20 08:59:02 +0530

    Time: 0 ms (100%), Space: 42.3 MB (53.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) return 1;
-
         long power = n;
         if (power < 0) {
             x = 1 / x;
@@ -10,11 +8,11 @@ class Solution {
 
         double result = 1;
         while (power > 0) {
-            if ((power & 1) == 1) {
+            if (power % 2 == 1) {
                 result *= x;
             }
-            x *= x;
-            power >>= 1;  // Same as power /= 2
+            x = x * x;
+            power = power / 2;
         }
         return result;
     }
